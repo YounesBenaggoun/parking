754d70768edb883c4175a7468b42cc1787e71f4d
Younes Benaggoun <p.primme@gmail.com>
2026-01-24 11:45:03 +0100

    first

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-Begin
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="./main.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    bonjour
+
+</body>
+
+</html>
