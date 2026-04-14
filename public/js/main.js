7eab695c0c9a7f57c5f9adf6c165c91415cd3f4c
Younes Benaggoun <younes.benaggoun3@gmail.com>
2026-04-14 11:32:41 +0200

    Marker Bind Popup

--- public/js/main.js
+++ public/js/main.js
@@ -19,8 +19,23 @@ window.onload = function () {
 }
 
 class Marker {
+    obj;
     constructor(lat, lng) {
         this.obj = L.marker([lat, lng]).addTo(window.map);
+        this.obj.bindPopup("<b>Rich content</b><br>More info here");
+        this.initEvent();
     }
-    
+    initEvent() {
+        this.obj.on('mouseover', function (e) {
+            this.openPopup();
+        });
+
+        // Close when mouse leaves
+        this.obj.on('mouseout', function (e) {
+            this.closePopup();
+        });
+
+    }
+
+
 }
